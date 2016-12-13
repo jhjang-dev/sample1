3e9667253647eece25c4aa44fb53c21366f9f56c
Developer Jang <jhjang.dev@gmail.com>
2016-12-13 10:12:23 +0900

    edit index file

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+$id = 'jhjang';
+$passwd = '1234';
+$select_db = 'my_test';
+
+$link = mysql_connect('localhost',$id,$passwd);
+if(!$link){
+	die('Not connected : '. mysql_error());
+}
+$db = mysql_select_db($select_db,$link);
+
+if(!$db){
+	die("Can't use test_db : " . mysql_error());
+}
+
+$sql = "select * from test";
+$res = mysql_query($sql);
+while($row = mysql_fetch_array($res,MYSQL_ASSOC)){
+	print_r($row);
+}
+
